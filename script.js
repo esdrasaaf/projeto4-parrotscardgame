678e8fb4970cbfc443e82a037995ba64c58bf74a
Esdras Antero <esdrasanterofontes@gmail.com>
2022-08-22 11:03:50 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //Início do fim
+let playerName = prompt("Qual o seu nome?")
+
 let cardsqntd = Number(prompt(`Com quantas cartas você deseja jogar? 
 Digite apenas números pares de 4 a 14`))
 
@@ -6,6 +8,22 @@ while (cardsqntd < 4 || cardsqntd > 14 || cardsqntd % 2 !== 0){
    cardsqntd = Number(prompt("Digite apenas números pares de 4 a 14!"))
 }
 //Fim do início do fim
+
+//Inicio do relogio
+let segundos = 0
+
+function timer(){
+    const cartaCerta = document.querySelectorAll('.acertou')
+
+    this.loop = setInterval(() => {
+        const relogio = document.querySelector(".relogio")
+        let tempo = segundos
+        segundos = segundos + 1
+
+        relogio.innerHTML = `Tempo: ${tempo}`
+    }, 1000);
+}
+//Fim do relogio
 
 // Criando as cartas do jogo e algumas funcionalidades
 const gifs = [
@@ -34,12 +52,22 @@ function fimDoGame(){
     const cartasCertas = document.querySelectorAll('.acertou')
 
     if (cartasCertas.length === cardsqntd) {
+        clearInterval(this.loop);
+        
         setTimeout(()=> {
-            alert(`Parabéns você ganhou o jogo dos piriquito em ${njogadas} jogadas!`)
-        }, 300)
+            alert(`Parabéns ${playerName}, você ganhou o jogo dos piriquito em ${njogadas} jogadas!
+Seu tempo total foi de ${segundos} segundos!`)
 
+            const resposta = prompt(`Deseja jogar novamente?
+(Responda apenas com "sim" ou "não")`)
+
+            if (resposta === "sim") {
+                location.reload()
+            } 
+        }, 300)
         
     }
+
 }
 
 function checkPair () {
@@ -70,8 +98,6 @@ function checkPair () {
 
         }, 1000)   
     }
-
-    
 }
 
 function virarCarta({target}){
@@ -96,7 +122,7 @@ function creatCards (gif) {
     const front = createElement('div', 'face front');
     const back = createElement('div', 'face back');
 
-    front.style.backgroundImage = `url('../images/${gif}.gif')`
+    front.style.backgroundImage = `url('./images/${gif}.gif')`
 
     card.appendChild(front)
     card.appendChild(back)
@@ -108,9 +134,11 @@ function creatCards (gif) {
 }
 
 function iniciarJogo (){
-
+    const name = document.querySelector(".player")
     const cardsDuplicados = [ ... gifs, ... gifs ]
     const cardsEmbaralhados = cardsDuplicados.sort(() => Math.random() - 0.5)
+
+    name.innerHTML = `${playerName}`
 
     cardsEmbaralhados.forEach((gif) => {
         const card = creatCards (gif);
@@ -120,5 +148,5 @@ function iniciarJogo (){
     })
 }
 // Terminei de criar as cartas do jogo e o fim do mesmo
-
+timer()
 iniciarJogo()
